6206bbed6183eed54479b2634216cc2495798012
Work desktop <the.red.lemon@yandex.ru>
2023-03-10 11:22:28 +0300

    feat: add loading data for profile

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,24 +1,41 @@
+import { useEffect, useState } from "react";
+import api from '../utils/Api.js';
+
 function Main(props) {
+
+  const [ userAvatar, setUserAvatar ] = useState(null),
+        [ userName, setUserName ] = useState(null),
+        [ userDescription, setUserDescription ] = useState(null);
+
+  useEffect(() => {
+    api.getUserInfo()
+      .then(userData => {
+        setUserAvatar(userData.avatar);
+        setUserName(userData.name);
+        setUserDescription(userData.about);
+      })
+      .catch(err => console.log(err));
+  })
 
   return (
     <main>
       <section className="profile container">
         <button
+          style={{ backgroundImage: `url(${userAvatar})` }}
           className="profile__btn-edit-avatar"
           onClick={() => props.onEditAvatar(true)}>
-          <img src="#" alt="#" className="profile__avatar" />
         </button>
 
         <div className="profile__info">
           <div className="profile__info-wrapper">
-            <h1 className="profile__title">Заголовок</h1>
+            <h1 className="profile__title">{userName}</h1>
             <button
               type="button"
               className="profile__edit-btn"
               onClick={() => props.onEditProfile(true)}>
             </button>
           </div>
-          <h2 className="profile__subtitle">Подзаголовок</h2>
+          <h2 className="profile__subtitle">{userDescription}</h2>
         </div>
         <button
           type="button"
---
+++ src/utils/constants.js
@@ -0,0 +1,62 @@
+export
+const apiSetting = {
+  baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-60',
+  headers: {
+    authorization: 'bdecdc76-75a5-40e2-94d6-35ac4e7b5bcc',
+    'Content-Type': 'application/json'
+  }
+}
+
+// export
+// const formSetting = {
+//       formSelector: '.popup__form',
+//       inputSelector: '.popup__input',
+//       submitButtonSelector: '.popup__submit-btn',
+//       inactiveButtonClass: 'popup__submit-btn_inactive',
+//       inputErrorClass: 'popup__input_type_error',
+//       errorClass: 'popup__input-error_active'
+// };
+
+// export
+// const cardSetting = {
+//       cardTemplate: '#card',
+//       cardSelector: '.card',
+//       cardImgSelector: '.card__img',
+//       cardTitleSelector: '.card__title',
+//       cardDeleteBtnSelector: '.card__delete',
+//       cardLikeBtnSelector: '.card__like-icon',
+//       activeLikeBtnClass: 'card__like-icon_active',
+//       cardLikeCounter: '.card__like-counter'
+// };
+
+// export
+// const userInfoData = {
+//       profileAvatarSelector: '.profile__avatar',
+//       profileTitleSelector: '.profile__title',
+//       profileSubtitleSelector: '.profile__subtitle'
+// };
+
+// export
+// const popupSelectors = {
+//       avatarEditPopup: '.popup[data-type="avatar-popup"]',
+//       profilePopup: '.popup[data-type="profile-popup"]',
+//       cardsPopup: '.popup[data-type="card-popup"]',
+//       imgPopup: '.popup[data-type="img-popup"]',
+//       confirmPopup: '.popup[data-type="confirm-popup"]'
+// }
+
+// export
+// const popupList = document.querySelectorAll('.popup'),
+//       formEditAvatar = document.forms['avatar-form'],
+//       avatarEditBtn = document.querySelector('.profile__btn-edit-avatar'),
+//       profileEditBtn = document.querySelector('.profile__edit-btn'),
+//       formEditProfile = document.forms['profile-form'],
+//       inputTitle = formEditProfile.querySelector('.popup__input_type_name'),
+//       inputSubtitle = formEditProfile.querySelector('.popup__input_type_about'),
+//       cardsAddBtn = document.querySelector('.profile__add-btn'),
+//       formAddCard = document.forms['card-form'],
+//       inputCardName = formAddCard.querySelector('.popup__input_type_name'),
+//       inputCardLink = formAddCard.querySelector('.popup__input_type_about'),
+//       cardsContainerSelector = '.cards__list',
+//       btnFormSelector = '.popup__submit-btn',
+//       btnLoadingText = 'Сохранение...';
