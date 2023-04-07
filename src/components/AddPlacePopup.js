685d880066a4863496412c43367c275e07e146b7
Work desktop <the.red.lemon@yandex.ru>
2023-04-07 10:32:10 +0300

    feat: add validate addPlacePopup form

--- src/components/AddPlacePopup.js
+++ src/components/AddPlacePopup.js
@@ -1,66 +1,58 @@
-import { useEffect, useState } from 'react';
 import PopupWithForm from './PopupWithForm';
+import { useFormValidation } from '../utils/useFormValidation';
 
 function AddPlacePopup({ isOpen, onClose, onAddPlace }) {
-  const [ title, setTitle ] = useState({}),
-        [ link, setLink ] = useState({});
-
-  useEffect(() => {
-    setTitle('');
-    setLink('');
-  }, [])
-
-  function handleChangeTitle({ target }) {
-    setTitle(target.value);
-  }
-
-  function handleChangeLink({ target }) {
-    setLink(target.value);
-  }
+  const { values, errors, isValid, handleChange, resetValues, formRef } = useFormValidation()
 
   function handleSubmit(e) {
     e.preventDefault();
 
     onAddPlace({
-      name: title,
-      link: link,
+      name: values['placeName'],
+      link: values['placeLink']
     });
+
+    resetValues();
   }
+
+  const errorClassName = (name) => `popup__input-error ${errors[name] ? 'popup__input-error_active' : ''}`
 
   return (
     <PopupWithForm
+      ref={formRef}
       name='card'
       title='Новое место'
       btnText='Создать'
       isOpen={isOpen}
       onClose={onClose}
-      onSubmit={handleSubmit}>
+      onSubmit={handleSubmit}
+      isValid={isValid}>
         <label className="popup__input-wrapper">
           <input
           type="text"
           id="input-img-title"
-          name="name"
+          name="placeName"
           className="popup__input popup__input_type_name"
           placeholder="Название"
           minLength="2"
           maxLength="30"
-          onChange={handleChangeTitle}
-          value={title}
+          onChange={handleChange}
+          value={values['placeName'] ?? ''}
           required />
-          <span className="popup__input-error input-img-title-error"></span>
+          <span className={errorClassName('placeName')}>{errors['placeName']}</span>
         </label>
 
         <label className="popup__input-wrapper">
           <input
           type="url"
           id="input-link"
-          name="link"
+          name="placeLink"
           className="popup__input popup__input_type_about"
           placeholder="Ссылка на картинку"
-          onChange={handleChangeLink}
-          value={link}
+          onChange={handleChange}
+          value={values['placeLink'] ?? ''}
           required/>
-          <span className="popup__input-error input-link-error"></span>
+          <span className={errorClassName('placeLink')}>{errors['placeLink']}</span>
         </label>
     </PopupWithForm>
   )
